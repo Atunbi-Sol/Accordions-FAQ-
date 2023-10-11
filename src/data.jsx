2bab97b5812c78feb653108a948a683d59f76b39
atunbi <damilareatunbi@gmail.com>
2023-10-11 14:28:37 +0100

    completed

---
+++ src/data.jsx
@@ -0,0 +1,32 @@
+export const dataCollection = [
+  {
+    question: "Can you explain what your name stands for?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam autem voluptatem, quasi soluta pariatur ullam accusamus officiis, ea eaque iure, explicabo vitae magni dignissimos! Error, dolorem nesciunt. Molestias, ex officiis! ",
+  },
+  {
+    question: "What makes blockchain trustworthy?",
+    answer:
+      "There are three sides to this answer. Firstly, nobody but the owner of the data can access it without permission. Thus, it is a safe way to protect data from third parties who should not see the information. Secondly, the data can be tracked at any time. It comes with a timestamp which indicates when the information was written on the blockchain and it is always available for fetching. ",
+  },
+  {
+    question: "Why Do You Want to Work at This Company?",
+    answer:
+      "Fusce et imperdiet ligula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras ipsum erat, ullamcorper nec bibendum aliquam, commodo ac enim. In a aliquet enim, nec vehicula ligula. Aenean non magna sapien. Integer vel massa vulputate, varius nunc nec, malesuada arcu. ",
+  },
+  {
+    question: "What Are Your Greatest Strengths?",
+    answer:
+      "Fusce et imperdiet ligula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras ipsum erat, ullamcorper nec bibendum aliquam, commodo ac enim. In a aliquet enim, nec vehicula ligula. Aenean non magna sapien. Integer vel massa vulputate, varius nunc nec, malesuada arcu. ",
+  },
+  {
+    question: "Can You Explain Why You Changed Career Paths?",
+    answer:
+      "Fusce et imperdiet ligula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras ipsum erat, ullamcorper nec bibendum aliquam, commodo ac enim. In a aliquet enim, nec vehicula ligula. Aenean non magna sapien. Integer vel massa vulputate, varius nunc nec, malesuada arcu. ",
+  },
+  {
+    question: "What could be the end of this world ?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quos, reprehenderit quibusdam commodi repellendus obcaecati doloremque dolorem exercitationem veritatis minus!. ",
+  },
+];
